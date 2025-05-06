de4674b84f0633de7dd0c9093736bb403605a041
mbakulev <michael.baculev@gmail.com>
2025-05-06 20:38:23 +0400

    add: метод вывода игрового поля

--- Tic-tac-homework/Program.cs
+++ Tic-tac-homework/Program.cs
@@ -2,6 +2,18 @@
 
 class Program
 {
+    static void ShowBoard(ref char[,] array)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(array[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Привет. Это игра крестики нолики!");

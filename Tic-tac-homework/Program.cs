cd2394dc5a3556a1d99507ebc148c8ad50a882e2
mbakulev <michael.baculev@gmail.com>
2025-05-06 20:35:58 +0400

    add:  информация об игроках

--- Tic-tac-homework/Program.cs
+++ Tic-tac-homework/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Привет. Это игра крестики нолики!");
+        
+        string playerOne = "1";
+        string playerTwo = "2";
+        
+        while (playerOne == null || playerOne == "")
+        {
+            Console.WriteLine("Введи имя первого игрока");
+            playerOne = Console.ReadLine();
+        }
+        
+        while (playerTwo == null || playerTwo == "")
+        {
+            Console.WriteLine("Введи имя второго игрока");
+            playerTwo = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Игрок 1 = " + playerOne);
+        Console.WriteLine("Игрок 2 = " + playerTwo);
     }
 }
